f4252b2908947fb8207ccb603da649c5bd202d3a
Dominik Engel <dominik.engel@mail.de>
2023-01-24 12:22:38 +0100

    WIP: improve gather_receiptive_fields

--- semisparseconv.py
+++ semisparseconv.py
@@ -10,3 +10,14 @@ def gather_receiptive_fields(volume, centers, ks=3):
             coord[1]-L:coord[1]+R,
             coord[2]-L:coord[2]+R] for coord in centers + ks
     ]).contiguous()
+
+def gather_receiptive_fields2(volume, centers, ks=3):
+    L = ks // 2
+    R = L+1
+    offsets = [[i,j,k] for i in range(-L,L+1) for j in range(-L, L+1) for k in range(-L, L+1)]
+    pad_vol = F.pad(volume, tuple([ks]*6))
+    return torch.stack([pad_vol[...,
+            centers + off[0],
+            coord[1]-L:coord[1]+R,
+            coord[2]-L:coord[2]+R] for coord in centers + ks
+    ]).contiguous()
