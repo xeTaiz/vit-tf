ce3454907f119eae3c4e0ea7790378a8dc1e403e
Dominik Engel <dominik.engel@mail.de>
2023-07-25 11:56:59 +0200

    use pretty printm and control num samples used for random forest and svm when not using exported annotations

--- predict_svm_rf.py
+++ predict_svm_rf.py
@@ -8,6 +8,7 @@ from sklearn.metrics import accuracy_score, precision_recall_fscore_support, jac
 
 from pathlib import Path
 from argparse import ArgumentParser
+from pprint import pprint
 
 from infer import make_3d, make_4d, make_5d, sample_features3d
 from compare_feat_sampling import sample_uniform, sample_surface, sample_both
@@ -74,18 +75,37 @@ if __name__ == '__main__':
     parser = ArgumentParser("Predict Segmentation using SVM and Random Forests")
     parser.add_argument('--data', type=str, required=True, help='Path to volume and annotation data')
     parser.add_argument('--svm-kernel', type=str, choices=['linear', 'poly', 'rgb', 'sigmoid', 'precomputed'], default='rbf', help='SVM Kernel function, see scikit-learn docs')
+    parser.add_argument('--num-samples', type=float, default=0.0, help='Number of samples to use for training, 0 to use annotations.npy')
     args = parser.parse_args()
 
     dir = Path(args.data)
     volume      = np.load(dir / 'volume.npy',      allow_pickle=True)  # (W,H,D,1)
-    annotations = np.load(dir / 'annotations.npy', allow_pickle=True)[()]  # { classname: (N, 3) }
     labels      = np.load(dir / 'labels.npy',      allow_pickle=True)  # (W,H,D)
+
+    if args.num_samples == 0.0:
+        annotations = np.load(dir / 'annotations.npy', allow_pickle=True)[()]  # { classname: (N, 3) }
+    elif args.num_samples > 1.0:
+        annotations = {}
+        for i in range(1, labels.max()+1):
+            mask = torch.as_tensor(labels == i)
+            N_SAMPLES = min(int(args.num_samples), mask.sum().item())
+            annotations[f'ntf{i}'] = sample_uniform(mask, N_SAMPLES, thin_to_reasonable=True)
+    elif args.num_samples > 0.0:
+        annotations = {}
+        for i in range(1, labels.max()+1):
+            mask = torch.as_tensor(labels == i)
+            N_SAMPLES = int(args.num_samples * mask.sum().item())
+            annotations[f'ntf{i}'] = sample_uniform(mask, N_SAMPLES, thin_to_reasonable=True)
+    else:
+        raise Exception(f'Invalid value for --num-samples: {args.num_samples}')
 
     print(f'Volume: {volume.shape}, {volume.dtype}')
     print(f'Labels: {labels.shape}, {labels.dtype}')
     print('Annotations: ', {k: v.shape for k, v in annotations.items()})
     print('Adding background annotations')
-    annotations['background'] = sample_uniform(labels == 0, 128, thin_to_reasonable=True)
+    BG_SAMPLES = max(list(map(lambda v: v.size(0), annotations.values()))) if args.num_samples != 0.0 else 128
+    annotations['background'] = sample_uniform(labels == 0, BG_SAMPLES, thin_to_reasonable=True)
+
     print('Annotations: ', {k: v.shape for k, v in annotations.items()})
     # input has 11-dim features containing intensity, grad mag, intensities of 6 neighbors and voxel coordinate, normalized to mean 0 std 1
     features = compose_features(torch.from_numpy(volume).float())
@@ -103,21 +123,50 @@ if __name__ == '__main__':
     t_svm_2 = time.time()
     print('SVM fit time:', t_svm_1 - t_svm_0)
     print('SVM predict time:', t_svm_2 - t_svm_1)
-    np.save(dir / 'svm_pred.npy', svm_pred)
+    np.save(dir / f'svm_pred{args.num_samples}.npy', svm_pred.reshape(labels.shape))
     prec, rec, f1, _ = precision_recall_fscore_support(labels.reshape(-1), svm_pred, average=None)
     cm = confusion_matrix(labels.reshape(-1), svm_pred)
     acc = cm.diagonal() / cm.sum(axis=1)
     iou = jaccard_score(labels.reshape(-1), svm_pred, average=None)
     svm_metrics = {
-        'accuracy': acc.tolist(),
-        'precision': prec.tolist(),
-        'recall': rec.tolist(),
-        'f1': f1.tolist(),
-        'iou': iou.tolist(),
-        'confusion_matrix': cm.tolist(),
+        'accuracy': dict(zip(annotations.keys(), acc.tolist())),
+        'precision': dict(zip(annotations.keys(), prec.tolist())),
+        'recall': dict(zip(annotations.keys(), rec.tolist())),
+        'f1': dict(zip(annotations.keys(), f1.tolist())),
+        'iou': dict(zip(annotations.keys(), iou.tolist())),
+        'confusion_matrix': dict(zip(annotations.keys(), cm.tolist())),
         'fit_time': t_svm_1 - t_svm_0,
         'predict_time': t_svm_2 - t_svm_1,
     }
-    print('SVM Metrics:', svm_metrics)
-    with open(dir / 'svm_metrics.json', 'w') as f:
+    print('SVM Metrics:')
+    pprint(svm_metrics)
+    with open(dir / f'svm_metrics{args.num_samples}.json', 'w') as f:
         json.dump(svm_metrics, f)
+
+    clf = RandomForestClassifier(n_estimators=32)
+    t_rf_0 = time.time()
+    clf.fit(train_X, train_y)
+    t_rf_1 = time.time()
+    rf_pred = clf.predict(features.permute(1,2,3,0).reshape(-1, features.shape[0]).numpy())
+    t_rf_2 = time.time()
+    print('RF fit time:', t_rf_1 - t_rf_0)
+    print('RF predict time:', t_rf_2 - t_rf_1)
+    np.save(dir / f'rf_pred{args.num_samples}.npy', rf_pred.reshape(labels.shape))
+    prec, rec, f1, _ = precision_recall_fscore_support(labels.reshape(-1), rf_pred, average=None)
+    cm = confusion_matrix(labels.reshape(-1), rf_pred)
+    acc = cm.diagonal() / cm.sum(axis=1)
+    iou = jaccard_score(labels.reshape(-1), rf_pred, average=None)
+    rf_metrics = {
+        'accuracy': dict(zip(annotations.keys(), acc.tolist())),
+        'precision': dict(zip(annotations.keys(), prec.tolist())),
+        'recall': dict(zip(annotations.keys(), rec.tolist())),
+        'f1': dict(zip(annotations.keys(), f1.tolist())),
+        'iou': dict(zip(annotations.keys(), iou.tolist())),
+        'confusion_matrix': dict(zip(annotations.keys(), cm.tolist())),
+        'fit_time': t_rf_1 - t_rf_0,
+        'predict_time': t_rf_2 - t_rf_1,
+    }
+    print('RF Metrics:')
+    pprint(rf_metrics)
+    with open(dir / f'rf_metrics{args.num_samples}.json', 'w') as f:
+        json.dump(rf_metrics, f)
