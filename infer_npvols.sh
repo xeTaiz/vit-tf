34af9a083fd48691792f9cee6ea06bca8bea7044
Dominik Engel <dominik.engel@mail.de>
2023-08-02 15:27:52 +0200

    add DINOv2 vits14 as well to infer script

--- infer_npvols.sh
+++ infer_npvols.sh
@@ -1,2 +1,3 @@
 find /mnt/hdd/dome/ntf_volumes -name '*.npy' -exec python infer.py --data-path {} --feature-output-size 128 --slice-along all \;
 find /mnt/hdd/dome/ntf_volumes -name '*.npy' -exec python infer.py --data-path {} --feature-output-size 128 --slice-along z \;
+find /mnt/hdd/dome/ntf_volumes -name '*.npy' -exec python infer.py --data-path {} --feature-output-size 128 --slice-along all --dino2-model vits14 \;
