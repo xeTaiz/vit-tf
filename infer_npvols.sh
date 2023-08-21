293982900ca8eda0a332fafbf6a598fe37f1f867
Dominik Engel <dominik.engel@uni-ulm.de>
2023-08-21 10:16:19 +0200

    infer script for feature size 64

--- infer_npvols.sh
+++ infer_npvols.sh
@@ -1,3 +1,1 @@
-find /mnt/hdd/dome/ntf_volumes -name '*.npy' -exec python infer.py --data-path {} --feature-output-size 128 --slice-along all \;
-find /mnt/hdd/dome/ntf_volumes -name '*.npy' -exec python infer.py --data-path {} --feature-output-size 128 --slice-along z \;
-find /mnt/hdd/dome/ntf_volumes -name '*.npy' -exec python infer.py --data-path {} --feature-output-size 128 --slice-along all --dino2-model vits14 \;
+find /mnt/hdd/dome/ntf_volumes/CT-ORG/npy -name 'volume.npy' -exec python infer.py --data-path {} --feature-output-size 64 --slice-along all \;
