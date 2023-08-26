987b12004a87f8d71587f0ad1674b4c72e3373bc
Dominik Engel <dominik.engel@mail.de>
2023-08-26 09:16:09 +0200

    add script to run svm /rf predictions for multiple num samples

---
+++ run_svm_rf_ctorg10b.sh
@@ -0,0 +1,5 @@
+python predict_svm_rf.py --data /run/media/dome/SSD/Data/Volumes/ctorg10b --num-samples 512 --sampling-mode uniform
+python predict_svm_rf.py --data /run/media/dome/SSD/Data/Volumes/ctorg10b --num-samples 1024 --sampling-mode uniform
+python predict_svm_rf.py --data /run/media/dome/SSD/Data/Volumes/ctorg10b --num-samples 2048 --sampling-mode uniform
+python predict_svm_rf.py --data /run/media/dome/SSD/Data/Volumes/ctorg10b --num-samples 4096 --sampling-mode uniform
+python predict_svm_rf.py --data /run/media/dome/SSD/Data/Volumes/ctorg10b --num-samples 8192 --sampling-mode uniform
