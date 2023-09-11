81a084fe137bb4f3b6bbabf00c362c6e85e6b663
Dominik Engel <dominik.engel@mail.de>
2023-09-11 10:51:32 +0200

    rename predict_similarities_ntf -> predict_ntf

--- sub/infer_predict_similarities1024.sh
+++ sub/infer_predict_similarities1024.sh
@@ -1,1 +1,1 @@
-while read i; do python -u predict_similarities_ntf.py --data /mnt/hdd/dome/ntf_volumes/CT-ORG/npy/$i --bilateral-solver --num-samples 1024; done <volumes_for_metrics.txt
+while read i; do python -u predict_ntf.py --data /mnt/hdd/dome/ntf_volumes/CT-ORG/npy/$i --bilateral-solver --num-samples 1024; done <volumes_for_metrics.txt
