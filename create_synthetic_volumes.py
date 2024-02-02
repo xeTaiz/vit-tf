b96d909cfaab874d75219137ab813b50e1866042
Dominik Engel <git@dominikengel.com>
2024-02-02 08:25:45 +0100

    create synthetic data script

---
+++ create_synthetic_volumes.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+def sdf_sphere(pos, r):
+    return torch.norm(pos, dim=-1) - r
+
+def sphere_thickness(pos, r, t):
+    return (torch.abs(sdf_sphere(pos, r)) < t).float()
+
+def sphere_filled(pos, r):
+    return (sdf_sphere(pos, r) <= 0).float()
+
+def sdf_torus(pos, r1, r2):
+    q = torch.norm(pos[..., :2], dim=-1) - r1
+    return torch.norm(torch.cat([q[..., None], pos[..., 2:]], dim=-1), dim=-1) - r2
+
+def torus_thickness(pos, r1, r2, t):
+    return (torch.abs(sdf_torus(pos, r1, r2)) < t).float()
+
+def torus_filled(pos, r1, r2):
+    return (sdf_torus(pos, r1, r2) <= 0).float()
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('outdir', type=Path, help='Output directory')
+    parser.add_argument('--size', type=int, default=128, help='Volume size')
+    parser.add_argument('--noise', type=float, default=0.0, help='Noise standard deviation')
+    parser.add_argument('--numpy', action='store_true', help='Save as numpy files')
+    args = parser.parse_args()
+
+    outdir = Path(args.outdir)
+    outdir.mkdir(exist_ok=True)
+
+    def noise_clamp(vol):
+        return torch.clamp(vol + torch.rand_like(vol) * args.noise, 0, 1)
+
+    def save_vol(vol, name):
+        if args.numpy:
+            np.save(outdir/f'{name}.npy', vol.numpy().astype(np.float16))
+        else:
+            torch.save(vol.to(torch.float16), outdir / f'{name}.pt')
+
+    ls = torch.linspace(-1, 1, args.size)
+    pos = torch.stack(torch.meshgrid(ls, ls, ls, indexing='xy'), dim=-1)
+
+    spheret = noise_clamp(sphere_thickness(pos, 0.5, 0.1))
+    spheref = noise_clamp(sphere_filled(pos, 0.5))
+    torust = noise_clamp(torus_thickness(pos, 0.5, 0.2, 0.1))
+    torusf = noise_clamp(torus_filled(pos, 0.5, 0.2))
+
+    save_vol(spheret, 'sphere_thickness')
+    save_vol(spheref, 'sphere_filled')
+    save_vol(torust, 'torus_thickness')
+    save_vol(torusf, 'torus_filled')
+
+if __name__ == '__main__':
+    main()
